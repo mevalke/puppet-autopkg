4052e3022c8bd6867a3cc77e90fa7bf4224b68dc
Miikka Valkeapaa <miikka1973@gmail.com>
2018-07-25 21:15:47 +0300

    initial commit

---
+++ manifests/autopkg_setup.pp
@@ -0,0 +1,84 @@
+define autopkg::autopkg_setup (
+  $munki_repo,
+  $autopkg_user,
+  $cache_dir,
+  $recipe_search_dirs,
+  $recipe_override_dirs,
+  $recipe_repo_dir,
+  $recipe_repos,
+  $recipes,
+  $recipe_arr,
+) {
+  exec { "Setting Autopkg Munki Repository $munki_repo":
+	  command => "/usr/bin/defaults write com.github.autopkg MUNKI_REPO -string $munki_repo",
+	  onlyif  => "ls /usr/local/bin/autopkg && ls $munki_repo",
+	  unless  => "/usr/bin/defaults read com.github.autopkg MUNKI_REPO|grep $munki_repo",
+	  timeout => 0,
+	  path    => '/bin:/usr/bin:/usr/local/bin',
+	  user    => $autopkg_user,
+  }
+  exec { "Setting Autopkg Cache $cache_dir":
+	  command => "/usr/bin/defaults write com.github.autopkg CACHE_DIR -string $cache_dir",
+	  onlyif  => "ls /usr/local/bin/autopkg && ls $cache_dir",
+	  unless  => "/usr/bin/defaults read com.github.autopkg CACHE_DIR|grep $cache_dir",
+	  timeout => 0,
+	  path    => '/bin:/usr/bin:/usr/local/bin',
+	  user    => $autopkg_user,
+  }
+  exec { "Setting Autopkg Search Directory $recipe_search_dirs":
+	  command => "/usr/bin/defaults write com.github.autopkg RECIPE_SEARCH_DIRS -string $recipe_search_dirs",
+	  onlyif  => "ls /usr/local/bin/autopkg && ls $recipe_search_dirs",
+	  unless  => "/usr/bin/defaults read com.github.autopkg RECIPE_SEARCH_DIRS|grep $recipe_search_dirs",
+	  timeout => 0,
+	  path    => '/bin:/usr/bin:/usr/local/bin',
+	  user    => $autopkg_user,
+  }
+  exec { "Setting Autopkg Recipe Override Directory $recipe_override_dirs":
+	  command => "/usr/bin/defaults write com.github.autopkg RECIPE_OVERRIDE_DIRS -string $recipe_override_dirs",
+	  onlyif  => "ls /usr/local/bin/autopkg && ls $recipe_override_dirs",
+	  unless  => "/usr/bin/defaults read com.github.autopkg RECIPE_OVERRIDE_DIRS|grep $recipe_override_dirs",
+	  timeout => 0,
+	  path    => '/bin:/usr/bin:/usr/local/bin',
+	  user    => $autopkg_user,
+  }
+  exec { "Setting Autopkg Repository Directory $recipe_repo_dir":
+	  command => "/usr/bin/defaults write com.github.autopkg RECIPE_REPO_DIR -string $recipe_repo_dir",
+	  onlyif  => "ls /usr/local/bin/autopkg && ls $recipe_repo_dir",
+	  unless  => "/usr/bin/defaults read com.github.autopkg RECIPE_REPO_DIR|grep $recipe_repo_dir",
+	  timeout => 0,
+	  path    => '/bin:/usr/bin:/usr/local/bin',
+	  user    => $autopkg_user,
+  }
+  exec {"Updating Autopkg Recipes for Munki Repository $munki_repo":
+    command => "/usr/local/bin/autopkg repo-add recipes",
+    onlyif  => "ls /usr/local/bin/autopkg",
+    timeout => 0,
+    path    => '/bin:/usr/bin:/usr/local/bin',
+    user    => $autopkg_user,
+  }
+  exec {"Adding additional Autopkg Recipes for Munki Repository $munki_repo":
+    command => "/usr/local/bin/autopkg repo-add $recipe_repos",
+    onlyif  => "ls /usr/local/bin/autopkg",
+    timeout => 0,
+    path    => '/bin:/usr/bin:/usr/local/bin',
+    user    => $autopkg_user,
+  }
+  $recipe_arr.each|$recipe| {
+    exec {"Adding Recipe Override $recipe_override_dirs/$recipe add_override":
+      command => "autopkg make-override $recipe",
+      unless  => "ls $recipe_override_dirs/$recipe",
+      timeout => 0,
+      path    => '/bin:/usr/bin:/usr/local/bin',
+      user    => $autopkg_user,
+      require => Exec["Adding additional Autopkg Recipes for Munki Repository $munki_repo"],
+    }
+  }    
+  exec {"Running Autopkg Recipes for Munki Repository $munki_repo":
+    command => "/usr/local/bin/autopkg run $recipes",
+    onlyif  => "ls /usr/local/bin/autopkg",
+    timeout => 0,
+    path    => '/bin:/usr/bin:/usr/local/bin',
+    user    => $autopkg_user,
+  }
+}
+
---
+++ manifests/init.pp
@@ -0,0 +1,67 @@
+class autopkg {
+  $autopkg_url	       = lookup(autopkg::autopkg_url)
+  $autopkg_folder      = "/Users/Shared/autopkg"
+  $autopkg_user        = lookup(autopkg::autopkg_user)
+  $autopkg_pw          = lookup(autopkg::autopkg_pw)
+  $autopkg_ssh_key     = lookup(autopkg::autopkg_ssh_key)
+  $repositories        = lookup(autopkg::repositories)
+  $munki_server        = lookup(autopkg::munki_server)
+
+
+  package {'autopkg':
+    ensure   => installed,
+    provider => pkgdmg,
+    source   => "$autopkg_url",
+  }
+
+  File {
+    owner => $autopkg_user,
+    group => admin,
+  }
+
+  file { "$autopkg_folder": ensure     => directory }
+  file { "$autopkg_folder/en": ensure  => directory }
+  file { "$autopkg_folder/fi": ensure  => directory }
+  file { "$autopkg_folder/log": ensure => file }
+
+  exec { 'tunnel_smb_through_ssh_to_munki_server':
+    command => "ssh -fNti /Users/$autopkg_user/.ssh/companykey -o 'StrictHostKeyChecking no' -L 139:127.0.0.1:139 $autopkg_user@$munki_server",
+    path    => '/bin:/usr/bin:/sbin',
+    unless  => "ps aux | grep '139:127.0.0.1:139 $autopkg_user@$munki_server'", 
+  }
+
+  $repositories.each|$repo, $param| {
+    file {"/Volumes/$repo":
+      ensure => directory,
+      owner  => $autopkg_user,
+    }
+    exec { "mount $repo":
+      command => "mount -t smbfs //$autopkg_user:$autopkg_pw@localhost/$repo /Volumes/$repo/",
+      path    => '/bin:/usr/bin:/sbin',
+      unless  => "ls /Volumes/$repo/pkgs",
+      user    => $autopkg_user,
+    }
+  }
+
+  $repositories.each|$repo, $param| {
+    $recipe_repos = $param['recipe_repos']
+    $recipes = $param['recipes']
+    $recipe_arr = split($recipes, ' ')
+    if $repo != 'en' {
+      $locale = $repo
+    } else {
+      $locale = undef
+    }
+    autopkg::autopkg_setup { $repo:
+      munki_repo           => "/Volumes/$repo",
+      autopkg_user         => $autopkg_user,
+      cache_dir            => "$autopkg_folder/$repo/Cache",
+      recipe_search_dirs   => "$autopkg_folder/$repo/Recipes",
+      recipe_override_dirs => "$autopkg_folder/$repo/RecipeOverrides",
+      recipe_repo_dir      => "$autopkg_folder/$repo/RecipeRepos",
+      recipe_repos         => $recipe_repos,
+      recipes              => "--post io.github.hjuutilainen.VirusTotalAnalyzer/VirusTotalAnalyzer --key LOCALE=$locale -k FAIL_RECIPES_WITHOUT_TRUST_INFO=no $recipes MakeCatalogs.munki.recipe",
+      recipe_arr           => $recipe_arr,
+    }
+  }
+}
